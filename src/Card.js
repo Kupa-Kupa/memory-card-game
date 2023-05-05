36666d2be5efb5e7a8865f3ddd944642ce7cf7ef
cooper <cooper.elliott30@gmail.com>
2023-05-05 13:54:50 +1000

    Add card shuffling

--- src/Card.js
+++ src/Card.js
@@ -49,6 +49,9 @@ const Card = (props) => {
         return card;
       });
 
+      // randomise order of new cards so they will display differently
+      shuffle(newCards);
+
       // set cards
       props.setCards(newCards);
     }
@@ -96,6 +99,21 @@ const Card = (props) => {
     // How do i force receive most recent value??
     if (props.score === 4) {
       console.log('You Win!');
+    }
+  };
+
+  // shuffle array function
+  // https://javascript.info/task/shuffle#:~:text=Write%20the%20function%20shuffle(array,%2C%202%5D%20%2F%2F%20...
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+      // swap elements array[i] and array[j]
+      // we use "destructuring assignment" syntax to achieve that
+      // you'll find more details about that syntax in later chapters
+      // same can be written as:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+      [array[i], array[j]] = [array[j], array[i]];
     }
   };
 
--- src/CardBoard.js
+++ src/CardBoard.js
@@ -4,18 +4,18 @@ import Card from './Card';
 
 const CardBoard = (props) => {
   const [cards, setCards] = useState([
-    { number: 546, clicked: false, id: 1 },
-    { number: 469, clicked: false, id: 2 },
-    { number: 120, clicked: false, id: 3 },
-    { number: 865, clicked: false, id: 4 },
-    { number: 564, clicked: false, id: 5 },
-    { number: 852, clicked: false, id: 6 },
-    { number: 613, clicked: false, id: 7 },
-    { number: 233, clicked: false, id: 8 },
-    { number: 176, clicked: false, id: 9 },
-    { number: 564, clicked: false, id: 10 },
-    { number: 136, clicked: false, id: 11 },
-    { number: 976, clicked: false, id: 12 },
+    { number: 1, clicked: false, id: 1 },
+    { number: 2, clicked: false, id: 2 },
+    { number: 3, clicked: false, id: 3 },
+    { number: 4, clicked: false, id: 4 },
+    { number: 5, clicked: false, id: 5 },
+    { number: 6, clicked: false, id: 6 },
+    { number: 7, clicked: false, id: 7 },
+    { number: 8, clicked: false, id: 8 },
+    { number: 9, clicked: false, id: 9 },
+    { number: 10, clicked: false, id: 10 },
+    { number: 11, clicked: false, id: 11 },
+    { number: 12, clicked: false, id: 12 },
   ]);
 
   if (Number(props.score) === 12) {
